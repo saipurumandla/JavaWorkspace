56be10b82d9e728c05817bb5f5852795b5a41aed
harinreddy <harinreddy94@gmail.com>
2017-05-08 22:44:55 -0400

    First Commit

---
+++ AdvancedJava/src/com/advancedjava/AdvancedMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.advancedjava;
+
+/**
+ * @author harin
+ *
+ */
+public class AdvancedMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
